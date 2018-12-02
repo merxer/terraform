b3f21177dd32032c3eb407c41a213ea0798846c0
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-02 18:30:36 +0700

    create couchdb and test data

--- ebs_encryption.tf
+++ ebs_encryption.tf
@@ -2,10 +2,36 @@
 provider "aws" {}
 
 resource "aws_instance" "couchdb" {
-  ami           = "ami-0eb1f21bbd66347fe"
-  instance_type = "t2.medium"
+  ami           = "ami-0c5199d385b432989"
+  instance_type = "t2.micro"
   key_name      = "merxer.io"
   availability_zone = "ap-southeast-1a"
+  user_data =<<-EOF
+	    #!/bin/bash
+	    apt update
+	    apt install -y apt-transport-https ca-certificates curl software-properties-common
+	    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+	    apt update
+	    apt-cache policy docker-ce
+	    apt install -y docker-ce
+	    # install docker compose
+	    curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	    chmod +x /usr/local/bin/docker-compose
+	    # start couchdb
+	    mkdir -p /opt/couchdb/data
+	    mkdir -p /opt/couchdb/etc/local.d
+	    docker run -p 5984:5984  -d \
+	    --volume /opt/couchdb/data:/opt/couchdb/data \
+	    --volume /opt/couchdb/etc/local.d:/opt/couchdb/etc/local.d  \
+	    --env COUCHDB_USER=admin \
+	    --env COUCHDB_PASSWORD=adminpassword \
+	    apache/couchdb:2.2
+	    # create database on couchdb
+	    curl -X PUT  http://admin:adminpassword@127.0.0.1:5984/fwd_db
+	    # insert data on couchdb
+	    curl -X PUT http://admin:adminpassword@127.0.0.1:5984/fwd_db/doc -d '{"data": "data test"}'
+	    EOF
 
   tags {
     Name = "Source"
